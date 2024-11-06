c17bcbfbdd2c7fb73c7157b1105de74d616061c6
Aditya Cholkar <adityacholkar@gmail.com>
2024-11-06 08:09:02 +0530

    Set up Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Nextjs</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-black">Welcome to the world of Nextjs</h1>
+      <h1 className="font-space-grotesk text-3xl font-black">Welcome to the world of Nextjs</h1>
+    </>
+  );
 }
