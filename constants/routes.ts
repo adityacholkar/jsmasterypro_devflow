e4f1d78e43133ea4dc7d39224759b8681ee444ee
Aditya Cholkar <adityacholkar@gmail.com>
2024-11-22 09:29:52 +0530

    Implement local search bar

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,8 +1,8 @@
-
 const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  ASK_QUESTION: "/ask-question",
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
 };
