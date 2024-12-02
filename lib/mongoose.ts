9d163039fe525ff382855411a00edd1efa92c7d8
Aditya Cholkar <adityacholkar@gmail.com>
2024-12-02 08:48:39 +0530

    Implement pino logger

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,9 +1,11 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./logger";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+  throw new Error("MONGODB_URI is not defined");
 }
 
 interface MongooseCache {
@@ -12,39 +14,39 @@ interface MongooseCache {
 }
 
 declare global {
+  // eslint-disable-next-line no-var
   var mongoose: MongooseCache;
 }
 
-let cached = global.mongoose as MongooseCache;
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = { conn: null, promise: null };
-  global.mongoose = cached;
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing mongoose connection");
     return cached.conn;
   }
 
   if (!cached.promise) {
-    const opts = {
-      dbName: "devflow",
-    };
-
     cached.promise = mongoose
-      .connect(MONGODB_URI, opts)
-      .then((mongoose) => {
-        console.log("Connected to MongoDB");
-        return mongoose;
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        logger.info("Connected to MongoDB");
+        return result;
       })
       .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
+        logger.error("Error connecting to MongoDB", error);
         throw error;
       });
   }
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
 
