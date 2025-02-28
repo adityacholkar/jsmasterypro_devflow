fe83be420f0267809f45fe3058826b3cd2deec34
Aditya Cholkar <adityacholkar@gmail.com>
2025-02-28 07:36:30 +0530

    Fetch question details and implement view increment functionality

--- lib/validations.ts
+++ lib/validations.ts
@@ -70,17 +70,12 @@ export const AskQuestionSchema = z.object({
 
 export const UserSchema = z.object({
   name: z.string().min(1, { message: "Name is required." }),
-  username: z
-    .string()
-    .min(3, { message: "Username must be at least 3 characters long." }),
+  username: z.string().min(3, { message: "Username must be at least 3 characters long." }),
   email: z.string().email({ message: "Please provide a valid email address." }),
   bio: z.string().optional(),
   image: z.string().url({ message: "Please provide a valid URL." }).optional(),
   location: z.string().optional(),
-  portfolio: z
-    .string()
-    .url({ message: "Please provide a valid URL." })
-    .optional(),
+  portfolio: z.string().url({ message: "Please provide a valid URL." }).optional(),
   reputation: z.number().optional(),
 });
 
@@ -104,24 +99,16 @@ export const AccountSchema = z.object({
     })
     .optional(),
   provider: z.string().min(1, { message: "Provider is required." }),
-  providerAccountId: z
-    .string()
-    .min(1, { message: "Provider Account ID is required." }),
+  providerAccountId: z.string().min(1, { message: "Provider Account ID is required." }),
 });
 
 export const SignInWithOAuthSchema = z.object({
   provider: z.enum(["google", "github"]),
-  providerAccountId: z
-    .string()
-    .min(1, { message: "Provider Account ID is required." }),
+  providerAccountId: z.string().min(1, { message: "Provider Account ID is required." }),
   user: z.object({
     name: z.string().min(1, { message: "Name is required." }),
-    username: z
-      .string()
-      .min(3, { message: "Username must be at least 3 characters long." }),
-    email: z
-      .string()
-      .email({ message: "Please provide a valid email address." }),
+    username: z.string().min(3, { message: "Username must be at least 3 characters long." }),
+    email: z.string().email({ message: "Please provide a valid email address." }),
     image: z.string().url("Invalid image URL").optional(),
   }),
 });
@@ -144,4 +131,8 @@ export const PaginatedSearchParamsSchema = z.object({
 
 export const GetTagQuestionsSchema = PaginatedSearchParamsSchema.extend({
   tagId: z.string().min(1, { message: "Tag ID is required." }),
+});
+
+export const IncrementViewsSchema = z.object({
+  questionId: z.string().min(1, { message: "Question ID is required." }),
 });
