239e54524ebb50c0d8c3aefd80e1f05cc3ff6f8b
Aditya Cholkar <adityacholkar@gmail.com>
2025-02-28 08:02:52 +0530

    Add AnswerForm component and validation for posting answers

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,5 +1,6 @@
 import TagCard from "@/components/cards/TagCard";
 import { Preview } from "@/components/editor/Preview";
+import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
@@ -75,6 +76,9 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           <TagCard key={tag._id} _id={tag._id as string} name={tag.name} compact />
         ))}
       </div>
+      <section className="my-5">
+        <AnswerForm />
+      </section>
     </>
   );
 };
