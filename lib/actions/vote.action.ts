3e2db0a6d92cb37cb13e61f0c13c0afd232c4a82
Aditya Cholkar <adityacholkar@gmail.com>
2025-03-24 07:30:52 +0530

    fix(votes): correct unauthorized error handling and update vote count logic

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -57,7 +57,7 @@ export async function createVote(params: CreateVoteParams): Promise<ActionRespon
   const { targetId, targetType, voteType } = validationResult.params!;
   const userId = validationResult.session?.user?.id;
 
-  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
 
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -77,6 +77,10 @@ export async function createVote(params: CreateVoteParams): Promise<ActionRespon
       } else {
         // If the user has already voted with a different voteType, update the vote
         await Vote.findByIdAndUpdate(existingVote._id, { voteType }, { new: true, session });
+        await updateVoteCount(
+          { targetId, targetType, voteType: existingVote.voteType, change: -1 },
+          session
+        );
         await updateVoteCount({ targetId, targetType, voteType, change: 1 }, session);
       }
     } else {
